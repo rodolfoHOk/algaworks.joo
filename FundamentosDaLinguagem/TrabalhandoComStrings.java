26b55cac6c7ee797f6900bfd23635554bda9b778
Rodolfo H Okino <rodolfo.okino@gmail.com>
2022-08-30 21:46:35 -0300

    Trabalhando com strings

---
+++ FundamentosDaLinguagem/TrabalhandoComStrings.java
@@ -0,0 +1,22 @@
+public class TrabalhandoComStrings {
+	
+	public static void main(String[] args) {
+	
+		System.out.println("Oi meu amigo/a do bem!");
+		
+		// concatenação de strings com +
+		int x = 10;
+		int y = 5;
+		int z = x + y;
+		System.out.println("Resultado: " + z); // imprime Resultado: 15
+		System.out.println("Resultado: " + x + y); // imprime Resultado: 105
+		System.out.println(x + y + " foi o resultado"); // imprime 15 foi o resultado
+		System.out.println("Resultado: " + (x + y)); // imprime Resultado: 15
+		
+		String nome = "Maria";
+		int idade = 30;
+		System.out.println(nome + " tem " + idade + " anos");
+		
+	}
+
+}
